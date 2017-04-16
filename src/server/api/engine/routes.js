315962b3f9f7516a27214951c819e47e15545243
John Leonard Tubig <jlu.tubig@gmail.com>
2017-04-16 23:41:12 +0800

    improve docus

--- src/server/api/engine/routes.js
+++ src/server/api/engine/routes.js
@@ -8,6 +8,24 @@ const log = require('debug')('api/engine/routes')
 
 const router = express.Router()
 
+/**
+ * @api {get} /api/engine Search
+ * @apName GetSearch
+ * @apiGroup Engine
+ *
+ * @apiParam {string} searchString
+ *
+ * @apiSuccess {object[]} result
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "result": [
+ *          { title: 'some-title', body: 'some-body', author: 'some-author' },
+ *          { title: 'some-title_2', body: 'some-body_2', author: 'some-author_2' }
+ *          { title: 'some-title_3', body: 'some-body_3', author: 'some-author_3' }
+ *        ]
+ *     }
+ */
 router.get('/', async (req, res) => {
   const searchString = req.query.searchString
   try {
