44cbdd7566ef04ad4ca0b6b47f1381bce5531c86
John Leonard Tubig <jlu.tubig@gmail.com>
2017-04-16 14:57:37 +0800

    copy google search

--- src/components/app/component.js
+++ src/components/app/component.js
@@ -1,19 +1,130 @@
 
 'use strict'
 
-import React from 'react'
+import _ from 'lodash'
+import React, { Component } from 'react'
 import styles from './style.scss'
+import cn from 'classnames'
+import logo from './assets/logo.png'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import SearchBar from 'Components/searchbar'
 
-const Component = props => {
-  return (
-    <MuiThemeProvider>
-      <div className={styles.root}>
-        <SearchBar />
+const log = require('debug')('components/app/component')
+
+class App extends Component {
+  state = {
+    results: [],
+    searching: false,
+    loading: false
+  }
+
+  onSearch = (searchString) => {
+    this.setState({
+      // clear previous result
+      results: [],
+      searching: true,
+      // Start loading
+      loading: true
+    })
+
+    log('onSearch', searchString)
+
+    // search command here
+    setTimeout(() => this.setState({
+      // disable loading
+      loading: false,
+      // render result
+      results: [
+        {
+          title: 'some title1',
+          body: 'some body1'
+        },
+        {
+          title: 'some title2',
+          body: 'some body2'
+        },
+        {
+          title: 'some title3',
+          body: 'some body3'
+        },{
+          title: 'some title4',
+          body: 'some body4'
+        }
+      ]
+    }), 2000);
+  }
+
+  getSearchbar = () => {
+    const { searching } = this.state
+    const { loading } = this.state
+    return (
+      <div className={cn({
+          [styles.searchbar]: true,
+          [styles[`searchbar-${searching ? 'searching': 'home'}`]]: true
+        })}>
+        <div className={styles['searchbar-logo']}>
+          <img
+            height={!searching ? 150 : 30}
+            src={logo}
+          />
+        </div>
+        <div className={styles[`searchbar-padding-${searching ? 'searching': 'home'}`]}/>
+        <SearchBar
+          onSearch={this.onSearch}
+          loading={loading}
+          showEasterEgg={!searching}
+        />
       </div>
-    </MuiThemeProvider>
-  )
+    )
+  }
+
+  getResults = () => {
+    const { results, loading } = this.state
+
+    if (_.isEmpty(results) || loading) return
+
+    return (
+      <div className={styles.results}>
+        Results here
+      </div>
+    )
+  }
+
+  getLoadingAnimation = () => {
+    const { loading } = this.state
+
+    if (!loading) return
+
+    return (
+      <div className={styles.loading}>
+        Loading ...
+      </div>
+    )
+  }
+
+  render () {
+    const { searching } = this.state
+    return (
+      <MuiThemeProvider>
+        <div
+          className={cn({
+            [styles.root]: true,
+            [styles['root-home']]: !searching
+          })}
+        >
+          {
+            this.getSearchbar()
+          }
+          {
+            this.getLoadingAnimation()
+          }
+          {
+            this.getResults()
+          }
+        </div>
+      </MuiThemeProvider>
+    )
+  }
 }
 
-export default Component
+export default App
