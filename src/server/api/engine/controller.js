e84a0afc6ae525cc1f5224025ff8877afb253f3a
ICSPCLAB01 <jlu.tubig@gmail.com>
2017-04-18 12:01:23 +0800

    create query

--- src/server/api/engine/controller.js
+++ src/server/api/engine/controller.js
@@ -4,10 +4,11 @@
 import connection from './connection'
 
 export function search (searchString, options) {
+  const MATCH_STRING = `MATCH(reviews.title, body) AGAINST('${searchString}')`
   return new Promise((resolve, reject) => {
     // Fancy querying here
     connection.query({
-      sql: 'SELECT * FROM posts'
+      sql: `SELECT reviews.id as id, reviews.title as title, body, reviews.author as author, books.title as bookTitle, books.author as bookAuthor, ${MATCH_STRING} as score FROM reviews JOIN books ON reviews.book_id=books.id WHERE ${MATCH_STRING} ORDER BY score DESC`
     }, (err, res) => {
       if (err) return reject(err)
       resolve(res)
