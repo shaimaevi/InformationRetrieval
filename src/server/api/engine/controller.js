79e6bd4a77245fd60c7177116837fbe97afae638
John Leonard Tubig <jlu.tubig@gmail.com>
2017-05-01 22:58:41 +0800

    prettify code

--- src/server/api/engine/controller.js
+++ src/server/api/engine/controller.js
@@ -13,18 +13,8 @@ export function search (searchString, options) {
 
   log(`MatchString for searchString '${searchString}': '${keywords}'`)
 
-  const SQL_STRING = `SELECT r.id as id, r.title as title, r.body as body, r.author as author, b.title as bookTitle, b.author as bookAuthor, ${MATCH_STRING} as score ` +
-                     `FROM reviews as r JOIN books as b ON r.book_id=b.id WHERE ${MATCH_STRING} ORDER BY score DESC`
+  const sql = `SELECT r.id as id, r.title as title, r.body as body, r.author as author, b.title as bookTitle, b.author as bookAuthor, ${MATCH_STRING} as score ` +
+              `FROM reviews as r JOIN books as b ON r.book_id=b.id WHERE ${MATCH_STRING} ORDER BY score DESC`
 
-  // log(SQL_STRING)
-
-  return new Promise((resolve, reject) => {
-    // Fancy querying here
-    connection.query({
-      sql: SQL_STRING
-    }, (err, res) => {
-      if (err) return reject(err)
-      resolve(res)
-    })
-  })
+  return new Promise((resolve, reject) => connection.query({ sql }, (err, res) => err ? reject(err) : resolve(res)))
 }
