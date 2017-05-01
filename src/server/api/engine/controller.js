98295898c6e1215823baa41eb64dc8949c1d9bc5
John Leonard Tubig <jlu.tubig@gmail.com>
2017-05-01 22:41:19 +0800

    log sql string

--- src/server/api/engine/controller.js
+++ src/server/api/engine/controller.js
@@ -7,15 +7,21 @@ import connection from './connection'
 const log = require('debug')('api/engine/controller')
 
 export function search (searchString, options) {
-  const keywords = _.words(searchString).map(k => `+${k}`).join(' ')
+  const keywords = _.words(searchString).map(k => `${k}`).join(' ')
 
-  log(`keywords for searchString '${searchString}' '${keywords}'`)
+  const MATCH_STRING = `MATCH(r.title, r.body) AGAINST('${keywords}')`
 
-  const MATCH_STRING = `MATCH(reviews.title, body) AGAINST('${keywords}')`
+  log(`MatchString for searchString '${searchString}': '${keywords}'`)
+
+  const SQL_STRING = `SELECT r.id as id, r.title as title, r.body as body, r.author as author, b.title as bookTitle, b.author as bookAuthor, ${MATCH_STRING} as score ` +
+                     `FROM reviews as r JOIN books as b ON r.book_id=b.id WHERE ${MATCH_STRING} ORDER BY score DESC`
+
+  log(SQL_STRING)
+
   return new Promise((resolve, reject) => {
     // Fancy querying here
     connection.query({
-      sql: `SELECT reviews.id as id, reviews.title as title, body, reviews.author as author, books.title as bookTitle, books.author as bookAuthor, ${MATCH_STRING} as score FROM reviews JOIN books ON reviews.book_id=books.id WHERE ${MATCH_STRING} ORDER BY score DESC`
+      sql: SQL_STRING
     }, (err, res) => {
       if (err) return reject(err)
       resolve(res)
